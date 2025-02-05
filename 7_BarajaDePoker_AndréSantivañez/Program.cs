0ca764a1ae8f4377f2b785e820998273ce736281
ASD-360 <android64sade@gmail.com>
2025-02-05 10:29:44 +0100

    Agregar archivos de proyecto.

---
+++ 7_BarajaDePoker_AndréSantivañez/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_BarajaDePoker_AndréSantivañez
+{
+    class Carta
+    {
+        public string Palo { get; set; }
+        public string Valor { get; set; }
+
+        // El constructor
+        public Carta(string Palo, string Valor)
+        {
+            Palo = palo;
+            Valor = valor;
+        }
+        public void MostrarCarta()
+        {
+            Console.WriteLine($"{Valor} de {Palo})");
+        }
+    }
+    class Baraja
+    {
+        private List<Carta> cartas;
+        public Baraja()
+        {
+            string[] palos = { "Corazones", "Diamantes", "Tréboles", "Picas" };
+            string[] valores = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            cartas = new List<Carta>();
+
+            foreach (string palo in palos)
+            {
+                foreach (string valor in valores)
+                {
+                    cartas.Add(new Carta(palo, valor));
+                }
+            }
+        }
+    }
+}
